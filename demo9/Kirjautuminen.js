6d8dbc9a1b728e4d3f443bae42f79ebf5492c02d
Benj4m1n1 <eelis.rajamaki@edu.keuda.fi>
2022-03-07 22:38:13 +0200

    update 6.969

---
+++ demo9/Kirjautuminen.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut() {
+    if (localStorage.getItem("kirjautunut") === 'kylla') {
+        document.getElementById('tervetulo_tekti').textContent += localStorage.getItem("nimi");
+        document.getElementById("kirjautumis_lomake").style.display = "none";
+
+    } else if (localStorage.getItem("kirjautunut") === 'ei') {
+
+        document.getElementById("kirjautumis_lomake").style.display = "table";
+        document.getElementById("uloskirjautumis_lomake").style.display = "none";
+    }
+}
+function kirjaudu() {
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+    localStorage.setItem("kirjautunut", "kylla");
+}
+function kirjauduUlos() {
+localStorage.removeItem("nimi");
+localStorage.setItem("kirjautunut", "ei");
+}
