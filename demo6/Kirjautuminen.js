c438197159dbac0d891e059ab330ef5197888cfe
Benj4m1n1 <99177972+Benj4m1n1@users.noreply.github.com>
2022-03-07 13:42:22 +0200

    Update Kirjautuminen.js

--- demo6/Kirjautuminen.js
+++ demo6/Kirjautuminen.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoader", onkoKirjautunut);
 function onkoKirjautunut() {
     let kirjautunut = 'kylla';
     if (kirjautunut === 'kylla'){
-        document.getElementById('Tervetulo_teksti').textContent += ' Eelis!';
+        document.getElementById('tervetulo_tekti').textContent += ' Eelis!';
     }
 }
