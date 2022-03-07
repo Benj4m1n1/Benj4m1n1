fd320ac7a0ef7274e721f88571103c8618c750ae
Benj4m1n1 <99177972+Benj4m1n1@users.noreply.github.com>
2022-03-07 14:42:50 +0200

    upload demo7

--- demo7/Kirjautuminen.js
+++ demo7/Kirjautuminen.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut() {
     let kirjautunut = 'kylla';
     if (kirjautunut === 'kylla') {
-        document.getElementById('tervetulo_tekti').textContent += ' Eelis!';
+        document.getElementById('tervetulo_tekti').textContent += localStorage.getItem("nimi");
     }
 }
-function kirjaudu(){
-    localStorage.setItem("nimi", "otto");
+function kirjaudu() {
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+    
 }
