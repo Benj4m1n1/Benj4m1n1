580605c1585e645c789360fa3847be8ee1da412d
Benj4m1n1 <99177972+Benj4m1n1@users.noreply.github.com>
2022-03-08 13:26:32 +0200

    dem 9 ff

--- demo9/Kirjautuminen.js
+++ demo9/Kirjautuminen.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
+
 function onkoKirjautunut() {
     if (localStorage.getItem("kirjautunut") === 'kylla') {
         document.getElementById('tervetulo_tekti').textContent += localStorage.getItem("nimi");
         document.getElementById("kirjautumis_lomake").style.display = "none";
 
-    } else if (localStorage.getItem("kirjautunut") === 'ei') {
+    } else {
 
         document.getElementById("kirjautumis_lomake").style.display = "table";
         document.getElementById("uloskirjautumis_lomake").style.display = "none";
